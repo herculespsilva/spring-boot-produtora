ebe427031cbae67884351f7cd80550a9934b55f0
JohnnyHTK <jonathanmmartins02@gmail.com>
2020-09-24 20:53:22 +0000

    Configurando Logback.xml #11

--- src/main/java/br/gov/sp/fatec/springbootprodutora/entity/Main.java
+++ src/main/java/br/gov/sp/fatec/springbootprodutora/entity/Main.java
@@ -5,8 +5,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @MappedSuperclass
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
