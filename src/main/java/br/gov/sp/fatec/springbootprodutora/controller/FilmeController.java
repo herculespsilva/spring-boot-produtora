7e99aa39aa0ba8cbf0bccfa23f1a460af37fcf70
JohnnyHTK <jonathanmmartins02@gmail.com>
2020-09-27 15:44:07 +0000

    Configurando POST #5

--- src/main/java/br/gov/sp/fatec/springbootprodutora/controller/FilmeController.java
+++ src/main/java/br/gov/sp/fatec/springbootprodutora/controller/FilmeController.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.gov.sp.fatec.springbootprodutora.entity.Filme;
 import br.gov.sp.fatec.springbootprodutora.service.SegurancaService;
 
 @RestController
 @RequestMapping(value = "/filme")
-@CrossOrigin
+@CrossOrigin //permite acessar de qualquer servidor
 
 public class FilmeController {
 
@@ -27,9 +29,19 @@ public class FilmeController {
     }
     
     @GetMapping(value="/{id}")
-    public Filme buscarPorID(@PathVariable("id") Long id){
+    
+    public Filme buscarPorID(@PathVariable("id") Long id){ //public Filme buscarPorID(@RequestParam(value="id") Long id) <--- Outra forma de receber valores via GET mas chama na URL /id?id=valor
         return segurancaService.buscarFilmeId(id);
     }
+
+    /*
+    @PostMapping
+    public Filme cadastrarFilme(@RequestBody Filme filme){
+        Ator ator ;
+        Duble duble;
+        Diretor diretor;
+        return segurancaService.criaFilme(filme.getNome(), filme.getAno(), filme.getDuracao(), filme.getDescricao(), filme.getPessoas(), ator.getNome(), duble.getNome());
+    }*/
 
     
 }
