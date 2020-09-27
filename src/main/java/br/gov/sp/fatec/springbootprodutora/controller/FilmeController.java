5a84beab6a3865eac4d946c0692149852c57fbad
JohnnyHTK <jonathanmmartins02@gmail.com>
2020-09-27 13:36:42 +0000

    Classes REST #5

---
+++ src/main/java/br/gov/sp/fatec/springbootprodutora/controller/FilmeController.java
@@ -0,0 +1,11 @@
+package br.gov.sp.fatec.springbootprodutora.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/filme")
+
+public class FilmeController {
+    
+}
