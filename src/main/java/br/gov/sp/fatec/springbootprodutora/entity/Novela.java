0c7dff3de99c27d966d6d6a62904598153c9be7c
VictorCardial <victor_cardial@hotmail.com>
2020-10-02 16:05:40 +0000

    View Novela

--- src/main/java/br/gov/sp/fatec/springbootprodutora/entity/Novela.java
+++ src/main/java/br/gov/sp/fatec/springbootprodutora/entity/Novela.java
@@ -4,13 +4,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
+import br.gov.sp.fatec.springbootprodutora.controller.View;
+
 @Entity
 @Table(name="nov_novela")
 public class Novela extends Filmagem{
-	
+    
+    
+    @JsonView(View.Novela.class)
 	@Column (name="nov_capitulo")
 	private Long capitulo;
-	
+    
+    @JsonView(View.Novela.class)
 	@Column (name="nov_desc_cap", length = 100)
 	private String descricaoCap;
 
