40d7bd3617e1b607a8ed1e4d133317052c4e24f8
JohnnyHTK <jonathanmmartins02@gmail.com>
2020-10-31 18:22:24 -0300

    script aws

---
+++ scripts/load_image.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker load -i /home/ec2-user/spring-app-produtora.tar.gz
---
+++ scripts/start_container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run -d --name spring-app-produtora
